b78aa4a3af6b68cc97c8909f4a2c7edd8a4348ae
Jay Lyonns <jaylyonns@gmail.com>
2018-07-13 00:18:53 +0000

    Upload: scripts/forms.js

--- scripts/forms.js
+++ scripts/forms.js
@@ -20,23 +20,23 @@ function submitForm(e) {
   e.preventDefault();
 
   // Get orderform values
-  var quantity01 = getInputVal("quantity01");
-  var quantity02 = getInputVal("quantity02");
-  var quantity03 = getInputVal("quantity03");
-  var quantity04 = getInputVal("quantity04");
-  var quantity05 = getInputVal("quantity05");
-  var quantity06 = getInputVal("quantity06");
-  var quantity07 = getInputVal("quantity07");
-  var quantity08 = getInputVal("quantity08");
-  var quantity09 = getInputVal("quantity09");
-  var quantity10 = getInputVal("quantity10");
-  var quantity11 = getInputVal("quantity11");
-  var quantity12 = getInputVal("quantity12");
-  var quantity13 = getInputVal("quantity13");
-  var quantity14 = getInputVal("quantity14");
-  var quantity15 = getInputVal("quantity15");
-  var quantity16 = getInputVal("quantity16");
-  var quantity17 = getInputVal("quantity17");
+  var qty01 = getInputVal("qty01");
+  var qty02 = getInputVal("qty02");
+  var qty03 = getInputVal("qty03");
+  var qty04 = getInputVal("qty04");
+  var qty05 = getInputVal("qty05");
+  var qty06 = getInputVal("qty06");
+  var qty07 = getInputVal("qty07");
+  var qty08 = getInputVal("qty08");
+  var qty09 = getInputVal("qty09");
+  var qty10 = getInputVal("qty10");
+  var qty11 = getInputVal("qty11");
+  var qty12 = getInputVal("qty12");
+  var qty13 = getInputVal("qty13");
+  var qty14 = getInputVal("qty14");
+  var qty15 = getInputVal("qty15");
+  var qty16 = getInputVal("qty16");
+  var qty17 = getInputVal("qty17");
   // Get message values
   var name = getInputVal("name");
   var company = getInputVal("company");
@@ -46,23 +46,23 @@ function submitForm(e) {
 
   // Save message
   saveMessage(
-    quantity01,
-    quantity02,
-    quantity03,
-    quantity04,
-    quantity05,
-    quantity06,
-    quantity07,
-    quantity08,
-    quantity09,
-    quantity10,
-    quantity11,
-    quantity12,
-    quantity13,
-    quantity14,
-    quantity15,
-    quantity16,
-    quantity17,
+    qty01,
+    qty02,
+    qty03,
+    qty04,
+    qty05,
+    qty06,
+    qty07,
+    qty08,
+    qty09,
+    qty10,
+    qty11,
+    qty12,
+    qty13,
+    qty14,
+    qty15,
+    qty16,
+    qty17,
     name,
     company,
     email,
@@ -89,23 +89,23 @@ function getInputVal(id) {
 
 // Save message to firebase
 function saveMessage(
-  quantity01,
-  quantity02,
-  quantity03,
-  quantity04,
-  quantity05,
-  quantity06,
-  quantity07,
-  quantity08,
-  quantity09,
-  quantity10,
-  quantity11,
-  quantity12,
-  quantity13,
-  quantity14,
-  quantity15,
-  quantity16,
-  quantity17,
+  qty01,
+  qty02,
+  qty03,
+  qty04,
+  qty05,
+  qty06,
+  qty07,
+  qty08,
+  qty09,
+  qty10,
+  qty11,
+  qty12,
+  qty13,
+  qty14,
+  qty15,
+  qty16,
+  qty17,
   name,
   company,
   email,
@@ -114,23 +114,23 @@ function saveMessage(
 ) {
   var newMessageRef = messagesRef.push();
   newMessageRef.set({
-    quantity01_RD12_1W_WW: quantity01,
-    quantity02_RD12_1W_CW: quantity02,
-    quantity03_RD12_RD_BK: quantity03,
-    quantity04_RD12_SQ_BK: quantity04,
-    quantity05_RD12_RD_WT: quantity05,
-    quantity06_RD12_SQ_WT: quantity06,
-    quantity07_RD12_RD_BN: quantity07,
-    quantity08_RD12_SQ_BN: quantity08,
-    quantity09_LS_15W_Driver: quantity09,
-    quantity10_LS_30W_Driver: quantity10,
-    quantity11_LS_60W_Driver: quantity11,
-    quantity12_LS_24D_CV: quantity12,
-    quantity13_LS45D_CV: quantity13,
-    quantity14_LS60D_CV: quantity14,
-    quantity15_WIRE18_2x10: quantity15,
-    quantity16_WIRE18_2x25: quantity16,
-    quantity17_WIRE18_2x50: quantity17,
+    qty01_RD12_1W_WW: qty01,
+    qty02_RD12_1W_CW: qty02,
+    qty03_RD12_RD_BK: qty03,
+    qty04_RD12_SQ_BK: qty04,
+    qty05_RD12_RD_WT: qty05,
+    qty06_RD12_SQ_WT: qty06,
+    qty07_RD12_RD_BN: qty07,
+    qty08_RD12_SQ_BN: qty08,
+    qty09_LS_15W_Driver: qty09,
+    qty10_LS_30W_Driver: qty10,
+    qty11_LS_60W_Driver: qty11,
+    qty12_LS_24D_CV: qty12,
+    qty13_LS45D_CV: qty13,
+    qty14_LS60D_CV: qty14,
+    qty15_WIRE18_2x10: qty15,
+    qty16_WIRE18_2x25: qty16,
+    qty17_WIRE18_2x50: qty17,
     name: name,
     company: company,
     email: email,
